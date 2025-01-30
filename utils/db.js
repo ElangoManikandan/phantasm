421c6e1f23f7beb6ca8de69e514a9f0202f889ec
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-01-30 21:41:38 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2/promise');
+import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+let pool;
 
-module.exports = pool;
+export const getPool = () => {
+  if (!pool) {
+    pool = mysql.createPool({
+      host: process.env.TIDB_HOST,
+      user: process.env.TIDB_USER,
+      password: process.env.TIDB_PASSWORD,
+      database: process.env.TIDB_DATABASE,
+      ssl: { rejectUnauthorized: true },
+      waitForConnections: true,
+      connectionLimit: 10, // Adjust as needed
+      queueLimit: 0
+    });
+  }
+  return pool;
+};
