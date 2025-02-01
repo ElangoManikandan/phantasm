c02f4d749b74b78ce615ba7fbaa2c3ea621deac7
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-02-01 11:27:34 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
+  uri: 'mysql://43yxnpPZ3zo884a.root:<PASSWORD>@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/symposium_db?ssl={"rejectUnauthorized":true}',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
