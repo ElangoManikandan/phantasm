103d6d9246a34bee59750d69ae65308957adc7a9
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-02-01 11:29:45 +0530

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import QRCode from 'qrcode';
 import path from 'path';
 import fs from 'fs';
-import {pool} from '../../utils/db';
+import db from '../../utils/db';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
@@ -36,7 +36,7 @@ async function handleLogin(req, res) {
     }
     
     try {
-        const [users] = await pool.promise().query('SELECT * FROM users WHERE email = ?', [email]);
+        const [users] = await db.promise().query('SELECT * FROM users WHERE email = ?', [email]);
         if (users.length === 0) {
             return res.status(404).json({ error: 'User not found!' });
         }
@@ -74,14 +74,14 @@ async function handleRegister(req, res) {
     
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        const result = await pool.promise().query(
+        const result = await db.promise().query(
             'INSERT INTO users (name, college, year, email, password, accommodation, role) VALUES (?, ?, ?, ?, ?, ?, ?)',
             [name, college, year, email, hashedPassword, accommodation, role]
         );
         
         const userId = result[0].insertId;
         const qr_code_id = `PSM_${userId}`;
-        await pool.promise().query('UPDATE users SET qr_code_id = ? WHERE id = ?', [qr_code_id, userId]);
+        await db.promise().query('UPDATE users SET qr_code_id = ? WHERE id = ?', [qr_code_id, userId]);
         
         const qrCodePath = path.join(process.cwd(), 'public', 'qrcodes', `user_${userId}.png`);
         if (!fs.existsSync(path.dirname(qrCodePath))) {
@@ -119,7 +119,7 @@ async function handlePasswordReset(req, res) {
     }
     try {
         const hashedPassword = await bcrypt.hash(newPassword, 10);
-        const result = await pool.promise().query('UPDATE users SET password = ? WHERE email = ?', [hashedPassword, email]);
+        const result = await db.promise().query('UPDATE users SET password = ? WHERE email = ?', [hashedPassword, email]);
         if (result[0].affectedRows > 0) {
             return res.status(200).json({ success: true, message: 'Password successfully reset' });
         }
