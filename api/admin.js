64b51605fc462662dd5b5b4c9c12bf7c09398d15
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-02-05 23:56:33 +0530

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -2,9 +2,7 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import db from "../utils/db.js"; // Ensure correct path with .js extension
-import middleware from "../middleware.js";
-const { requireAuth, requireAdmin } = middleware;
-
+import auth from "../utils/auth.js";
 const router = express.Router();
 
 // Login route for admin
