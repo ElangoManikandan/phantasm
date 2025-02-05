a05554a376f51ca68f2ce2b63a067c41075afb38
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-02-05 23:38:02 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ import db from "../utils/db.js";
 import userRouter from './user.js'; 
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import {requireAuth,requireAdmin} from "./middleware.js";
+import {requireAuth} from "./middleware.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
