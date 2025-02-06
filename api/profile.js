4feee59ec1af1d31aac573d2a174e24fa0c95d1d
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-02-06 18:28:30 +0530

    Delete api/profile.js

--- api/profile.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import db from "../utils/db.js"; // Assuming db.query is your SQL query function
-import jwt from 'jsonwebtoken';
-import { requireAuth } from "./middleware.js"; 
-
-const router = express.Router();
-
