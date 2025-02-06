5cc5be90e1116489fddff9326de10656c25c9ff5
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-02-06 10:55:45 +0530

    Update user.js

--- api/user.js
+++ api/user.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import db from "../utils/db.js"; // Assuming db.query is your SQL query function
 import jwt from 'jsonwebtoken';
-import { requireAuth } from "../middleware.js"; 
+import { requireAuth } from "./middleware.js"; 
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET; // Use your secret for JWT
