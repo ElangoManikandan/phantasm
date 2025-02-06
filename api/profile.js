5d663dee69ec5900a288c5c4b7f5aef9344ff282
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-02-06 11:27:03 +0530

    Update profile.js

--- api/profile.js
+++ api/profile.js
@@ -4,33 +4,6 @@ import { requireAuth, requireAdmin } from "./middleware.js";
 
 
 const router = express.Router();
-
-// Get User Profile Route
-// Get Profile Route using JWT Authentication
-router.get("/get-profile", requireAuth, (req, res) => {
-    const userId = req.user.id; // Access user id from the JWT payload
-
-    // Query to fetch the user details
-    db.query(
-        "SELECT id, name, college, year, accommodation, role FROM users WHERE id = ?",
-        [userId],
-        (err, results) => {
-            if (err) {
-                console.error("Database error:", err);
-                return res.status(500).json({ error: "Database error!" });
-            }
-            if (results.length === 0) {
-                return res.status(404).json({ error: "User not found!" });
-            }
-
-            const user = results[0];
-            user.qr_code_id = `user_${user.id}.png`; // Dynamically add qr_code_id based on user id
-
-            res.json(user); // Send the updated user data
-        }
-    );
-});
-
 // Update User Profile Route
 // POST method for updating profile
 router.post("/update-profile", requireAuth, (req, res) => {
